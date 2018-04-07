108663e86997d68c16cebaf93edbdfe8a5136083
Robin Schoonover <robin@cornhooves.org>
2018-04-07 08:01:21 -0600

    Split push/run script task into own 'script' type.

--- bert/tasks.py
+++ bert/tasks.py
@@ -93,27 +93,9 @@ class TaskExportBin(Task, name="export-bin"):
 
 class TaskRun(Task, name="run"):
     def run(self, job):
-        if self.value.startswith("./"):
-            command = posixpath.join(job.work_dir, self.value)
-
-            container = job.create({
-                'value' : self.value,
-                'file_sha256' : file_hash('sha256', self.value)
-            }, command=command)
-
-            with tempfile.TemporaryFile() as tf:
-                with tarfile.open(fileobj=tf, mode="w") as tar:
-                    tar.add(self.value)
-                tf.seek(0)
-
-                container.put_archive(
-                    path=job.work_dir,
-                    data=tf
-                )
-        else:
-            container = job.create({
-                'value' : self.value
-            }, command=self.value)
+        container = job.create({
+            'value' : self.value
+        }, command=self.value)
 
         job.commit()
 
@@ -136,6 +118,27 @@ class TaskReadFile(Task, name="read-file"):
                     break
 
         job.cancel()
+
+class TaskScript(Task, name="script"):
+    def run(self, job):
+        command = posixpath.join(job.work_dir, self.value)
+
+        container = job.create({
+            'value' : self.value,
+            'file_sha256' : file_hash('sha256', self.value)
+        }, command=command)
+
+        with tempfile.TemporaryFile() as tf:
+            with tarfile.open(fileobj=tf, mode="w") as tar:
+                tar.add(self.value)
+            tf.seek(0)
+
+            container.put_archive(
+                path=job.work_dir,
+                data=tf
+            )
+
+        job.commit()
 
 class TaskSetup(Task, name="setup"):
     def run(self, job):
