4464f421445f9353c3d88651d84cb0f22d24e964
Robin Schoonover <robin@cornhooves.org>
2019-01-26 14:17:51 -0700

    git: If ref is known tag or commit hash, don't recheck remote

--- bert/tasks/git.py
+++ bert/tasks/git.py
@@ -51,6 +51,17 @@ class GitRun(object):
 
         self.job.commit()
 
+    def have_static_ref(self, dest, ref):
+        """
+        Determine is a reference is a tag or commit hash which
+        should not change.
+        """
+        if subprocess.call(["git", "show-ref", "-q", "--verify", "refs/tags/"+ref], cwd=dest) == 0:
+            return True
+        if subprocess.call(["git", "rev-parse", "-q", "--verify", ref+"^{commit}"], cwd=dest) == 0:
+            return True
+        return False
+
     def run_git(self, dest=None):
         have_fetch_head = True
 
@@ -60,21 +71,24 @@ class GitRun(object):
         if not os.path.isdir(dest):
             subprocess.check_call(["git", "clone", self.repo, dest])
 
-        try:
-            subprocess.check_call(["git", "fetch", self.repo, "--tags", self.ref], cwd=dest)
-        except subprocess.CalledProcessError:
+        if self.have_static_ref(dest, self.ref):
             have_fetch_head = False
-            # Some git repos forbit fetching via a commit hash, so retry
-            # with everything
-            print("Warning: fetching %s directly failed"%(self.ref, ))
-            subprocess.check_call(["git", "fetch", self.repo], cwd=dest)
+        else:
+            try:
+                subprocess.check_call(["git", "fetch", self.repo, "--tags", self.ref], cwd=dest)
+            except subprocess.CalledProcessError:
+                have_fetch_head = False
+                # Some git repos forbit fetching via a commit hash, so retry
+                # with everything
+                print("Warning: fetching %s directly failed"%(self.ref, ))
+                subprocess.check_call(["git", "fetch", self.repo], cwd=dest)
 
-        try:
-            if have_fetch_head:
-                subprocess.check_call(["git", "checkout", "FETCH_HEAD"], cwd=dest)
-        except subprocess.CalledProcessError:
-            print("Warning: %s not found at FETCH_HEAD, retrying directly"%(self.ref,))
-            have_fetch_head = False
+            try:
+                if have_fetch_head:
+                    subprocess.check_call(["git", "checkout", "FETCH_HEAD"], cwd=dest)
+            except subprocess.CalledProcessError:
+                print("Warning: %s not found at FETCH_HEAD, retrying directly"%(self.ref,))
+                have_fetch_head = False
 
         if not have_fetch_head:
             subprocess.check_call(["git", "checkout", self.ref], cwd=dest)
