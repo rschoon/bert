9277618e637c35fcf8d8a4eb0d177b70c9dd4bdb
Robin Schoonover <robin@cornhooves.org>
2019-05-04 15:38:19 -0600

    add: Set destination and mode

--- bert/tasks/add.py
+++ bert/tasks/add.py
@@ -1,10 +1,11 @@
 
 import os
+import posixpath
 import tarfile
 import tempfile
 
 from . import Task, TaskVar
-from ..utils import file_hash
+from ..utils import file_hash, expect_file_mode
 
 class TaskAdd(Task, name="add"):
     """
@@ -12,21 +13,50 @@ class TaskAdd(Task, name="add"):
     """
 
     class Schema:
-        path = TaskVar(bare=True, help="Path to local file to add")
+        path = TaskVar('src', bare=True, help="Path to local file to add")
+        dest = TaskVar('dest', help="Destination path of file")
+        mode = TaskVar(type=expect_file_mode, help="The unix file mode to use for the tar file.")
 
-    def run_with_values(self, job, path):
-        container = job.create({
+    def run_with_values(self, job, path, dest, mode):
+        job_args = {
             'value' : path,
             'file_sha256' : file_hash('sha256', path)
-        })
+        }
+        if mode is not None:
+            job_args['mode'] = mode
+
+        container = job.create(job_args)
+
+        arcname = os.path.basename(path)
+        if dest is None:
+            arcname = posixpath.join(job.work_dir, arcname)
+        else:
+            if dest.endswith("/"):
+                arcname = posixpath.join(dest, arcname)
+            else:
+                arcname = dest
 
         with tempfile.TemporaryFile() as tf:
             with tarfile.open(fileobj=tf, mode="w") as tar:
-                tar.add(path, arcname=os.path.basename(path))
+               ti = tar.gettarinfo(path, arcname)
+
+               if mode is not None:
+                   ti.mode = (ti.mode & ~0o777) | mode
+
+               if ti.isreg():
+                   with open(path, "rb") as fi:
+                       tar.addfile(ti, fi)
+               elif tarinfo.isdir():
+                   tar.addfile(ti)
+                   for fn in sorted(os.listdir(path)):
+                       tar.add(os.path.join(name, fn), posixpath.join(arcname, fn))
+               else:
+                   tar.addfile(ti)
+
             tf.seek(0)
 
             container.put_archive(
-                path=job.work_dir,
+                path="/",
                 data=tf
             )
 
